67386939077ce1deb009dfbee7b36bfd0856369d
Vinicius Rodrigues <viniciusr.camargo@gmail.com>
2022-12-28 18:20:28 -0300

    Learning explode and implode functions

--- usuario.php
+++ usuario.php
@@ -1,8 +1,13 @@
 <?php
 
+$nome = 'Vinícius Rodrigues';
 $email = 'vinicius@microsoft.com.br';
 $senha = 'áéúç';
+
+
 $posicaoDoArroba = strpos($email, '@');
+
+echo mb_strlen($senha);
 
 if(mb_strlen($senha) < 8){
     echo 'senha insegura' . PHP_EOL;
@@ -10,3 +15,11 @@ if(mb_strlen($senha) < 8){
 $usuario = substr($email,0,$posicaoDoArroba);
 echo strtoupper($usuario) . PHP_EOL;
 echo substr($email,$posicaoDoArroba + 1) . PHP_EOL;
+
+list($nome, $sobrenome) = explode(' ', $nome);
+echo 'Nome: ' . $nome . PHP_EOL;
+echo 'Sobrenome: ' . $sobrenome . PHP_EOL;
+
+$csv = 'Vinicius Rodrigues, 37, vinicius@gmail.com';
+var_dump(explode(',', $csv));
+//explode separa string em array separada por separador estabelecido
